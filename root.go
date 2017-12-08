9fff9ce790208f8f90dba6ceb4a228ec2d336fa6
Nico Schieder <Nico.schieder@gmail.com>
2017-12-08 13:24:35 +0100

    Implement bash/zsh completion

--- root.go
+++ root.go
@@ -4,13 +4,14 @@ import "github.com/spf13/cobra"
 
 func NewRootCommand(cli *CLI) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:              "hcloud",
-		Short:            "Hetzner Cloud CLI",
-		Long:             "A command-line interface for Hetzner Cloud",
-		RunE:             cli.wrap(runRoot),
-		TraverseChildren: true,
-		SilenceUsage:     true,
-		SilenceErrors:    true,
+		Use:                    "hcloud",
+		Short:                  "Hetzner Cloud CLI",
+		Long:                   "A command-line interface for Hetzner Cloud",
+		RunE:                   cli.wrap(runRoot),
+		TraverseChildren:       true,
+		SilenceUsage:           true,
+		SilenceErrors:          true,
+		BashCompletionFunction: bashCompletionFunc,
 	}
 	cmd.Flags().BoolVar(&cli.JSON, "json", false, "Output JSON API response")
 	cmd.AddCommand(
@@ -19,6 +20,7 @@ func NewRootCommand(cli *CLI) *cobra.Command {
 		newServerCommand(cli),
 		newSSHKeyCommand(cli),
 		newVersionCommand(cli),
+		newCompletionCommand(cli),
 	)
 	return cmd
 }
