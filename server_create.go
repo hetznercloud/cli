676173e695d5ed5ff582429c66fcd614a9daaa96
Thomas Cyron <thomas@thcyron.de>
2017-12-18 16:22:04 +0100

    Support creating server with SSH keys

--- server_create.go
+++ server_create.go
@@ -3,7 +3,6 @@ package cli
 import (
 	"context"
 	"fmt"
-	"time"
 
 	"github.com/hetznercloud/hcloud-go/hcloud"
 	"github.com/spf13/cobra"
@@ -20,6 +19,7 @@ func newServerCreateCommand(cli *CLI) *cobra.Command {
 	cmd.Flags().String("name", "", "Server name")
 	cmd.Flags().String("type", "", "Server type (id or name)")
 	cmd.Flags().String("image", "", "Image (id or name)")
+	cmd.Flags().IntSlice("ssh-key", nil, "ID of SSH key to inject (can be specified multiple times)")
 	return cmd
 }
 
@@ -29,6 +29,7 @@ func runServerCreate(cli *CLI, cmd *cobra.Command, args []string) error {
 	name, _ := cmd.Flags().GetString("name")
 	serverType, _ := cmd.Flags().GetString("type")
 	image, _ := cmd.Flags().GetString("image")
+	sshKeys, _ := cmd.Flags().GetIntSlice("ssh-key")
 
 	opts := hcloud.ServerCreateOpts{
 		Name: name,
@@ -39,44 +40,23 @@ func runServerCreate(cli *CLI, cmd *cobra.Command, args []string) error {
 			Name: image,
 		},
 	}
+	for _, sshKey := range sshKeys {
+		opts.SSHKeys = append(opts.SSHKeys, &hcloud.SSHKey{ID: sshKey})
+	}
 
 	result, _, err := cli.Client().Server.Create(ctx, opts)
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("Server %d created\n", result.Server.ID)
-	initStarted := time.Now()
-	terminal := cli.Terminal()
-
-	if terminal {
-		fmt.Println("Initializing server ...")
+	if result.RootPassword != "" {
+		fmt.Printf("Server %d created with root password: %s\n", result.Server.ID, result.RootPassword)
+	} else {
+		fmt.Printf("Server %d created\n", result.Server.ID)
 	}
 
-progress:
-	for {
-		action, _, err := cli.Client().Action.Get(ctx, result.Action.ID)
-		if err != nil {
-			return err
-		}
-		switch action.Status {
-		case "running":
-			if terminal {
-				cli.ClearLine()
-				fmt.Printf("Initializing server ... %d%%\n", action.Progress)
-			}
-			time.Sleep(100 * time.Millisecond)
-		case "success":
-			duration := time.Now().Sub(initStarted).Round(time.Millisecond)
-			if terminal {
-				cli.ClearLine()
-			}
-			fmt.Printf("Server initialized in %s\n", duration)
-			break progress
-		case "error":
-			fmt.Println(action.ErrorMessage)
-			break progress
-		}
+	if err := <-waitAction(ctx, cli.Client(), result.Action); err != nil {
+		return err
 	}
 
 	return nil
