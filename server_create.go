75745102820a6e0d5cb288447eb282dad8f6196f
Thomas Cyron <thomas@thcyron.de>
2017-12-21 18:21:18 +0100

    Refactor creating server create options from flags

--- server_create.go
+++ server_create.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/hetznercloud/hcloud-go/hcloud"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 func newServerCreateCommand(cli *CLI) *cobra.Command {
@@ -26,12 +27,32 @@ func newServerCreateCommand(cli *CLI) *cobra.Command {
 }
 
 func runServerCreate(cli *CLI, cmd *cobra.Command, args []string) error {
-	name, _ := cmd.Flags().GetString("name")
-	serverType, _ := cmd.Flags().GetString("type")
-	image, _ := cmd.Flags().GetString("image")
-	location, _ := cmd.Flags().GetString("location")
-	datacenter, _ := cmd.Flags().GetString("datacenter")
-	sshKeys, _ := cmd.Flags().GetIntSlice("ssh-key")
+	opts := optsFromFlags(cmd.Flags())
+	result, _, err := cli.Client().Server.Create(cli.Context, opts)
+	if err != nil {
+		return err
+	}
+
+	if err := cli.ActionProgress(cli.Context, result.Action); err != nil {
+		return err
+	}
+
+	if result.RootPassword != "" {
+		fmt.Printf("Server %d created with root password: %s\n", result.Server.ID, result.RootPassword)
+	} else {
+		fmt.Printf("Server %d created\n", result.Server.ID)
+	}
+
+	return nil
+}
+
+func optsFromFlags(flags *pflag.FlagSet) hcloud.ServerCreateOpts {
+	name, _ := flags.GetString("name")
+	serverType, _ := flags.GetString("type")
+	image, _ := flags.GetString("image")
+	location, _ := flags.GetString("location")
+	datacenter, _ := flags.GetString("datacenter")
+	sshKeys, _ := flags.GetIntSlice("ssh-key")
 
 	opts := hcloud.ServerCreateOpts{
 		Name: name,
@@ -52,20 +73,5 @@ func runServerCreate(cli *CLI, cmd *cobra.Command, args []string) error {
 		opts.Location = &hcloud.Location{Name: location}
 	}
 
-	result, _, err := cli.Client().Server.Create(cli.Context, opts)
-	if err != nil {
-		return err
-	}
-
-	if err := cli.ActionProgress(cli.Context, result.Action); err != nil {
-		return err
-	}
-
-	if result.RootPassword != "" {
-		fmt.Printf("Server %d created with root password: %s\n", result.Server.ID, result.RootPassword)
-	} else {
-		fmt.Printf("Server %d created\n", result.Server.ID)
-	}
-
-	return nil
+	return opts
 }
