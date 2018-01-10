17d855178daf5b3ddfed46edd66995b9488a63af
Nico Schieder <nico.schieder@hetzner-cloud.de>
2018-01-10 15:38:15 +0100

    Add more fields to datacenter and server describe

--- datacenter_describe.go
+++ datacenter_describe.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"fmt"
 
+	"github.com/hetznercloud/hcloud-go/hcloud"
 	"github.com/spf13/cobra"
 )
 
@@ -38,21 +39,41 @@ func runDatacenterDescribe(cli *CLI, cmd *cobra.Command, args []string) error {
 	fmt.Printf("  City:\t\t%s\n", datacenter.Location.City)
 	fmt.Printf("  Latitude:\t%f\n", datacenter.Location.Latitude)
 	fmt.Printf("  Longitude:\t%f\n", datacenter.Location.Longitude)
-	fmt.Printf("ServerTypes:\n")
-	st := datacenter.ServerTypes
-	fmt.Printf("  Available:\n")
-	if len(st.Available) > 0 {
-		for _, t := range st.Available {
-			fmt.Printf("  - ID:\t%d\n", t.ID)
+	fmt.Printf("Server Types:\n")
+
+	serverTypesMap := map[int]*hcloud.ServerType{}
+	for _, t := range datacenter.ServerTypes.Available {
+		serverTypesMap[t.ID] = t
+	}
+	for _, t := range datacenter.ServerTypes.Supported {
+		serverTypesMap[t.ID] = t
+	}
+	for id := range serverTypesMap {
+		var err error
+		serverTypesMap[id], _, err = cli.client.ServerType.GetByID(cli.Context, id)
+		if err != nil {
+			return fmt.Errorf("error fetching server type: %v", err)
 		}
+	}
+
+	printServerTypes := func(list []*hcloud.ServerType, dataMap map[int]*hcloud.ServerType) {
+		for _, t := range list {
+			st := dataMap[t.ID]
+			fmt.Printf("  - ID:\t\t %d\n", st.ID)
+			fmt.Printf("    Name:\t %s\n", st.Name)
+			fmt.Printf("    Description: %s\n", st.Description)
+		}
+	}
+
+	fmt.Printf("  Available:\n")
+	if len(datacenter.ServerTypes.Available) > 0 {
+		printServerTypes(datacenter.ServerTypes.Available, serverTypesMap)
 	} else {
 		fmt.Printf("    No available server types\n")
 	}
 	fmt.Printf("  Supported:\n")
-	if len(st.Supported) > 0 {
-		for _, t := range st.Supported {
-			fmt.Printf("  - ID:\t%d\n", t.ID)
-		}
+	if len(datacenter.ServerTypes.Supported) > 0 {
+		printServerTypes(datacenter.ServerTypes.Available, serverTypesMap)
 	} else {
 		fmt.Printf("    No supported server types\n")
 	}
