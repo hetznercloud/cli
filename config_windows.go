8bd65b6972ec9530a80bf05bbd61a2ea5b1162b6
Thomas Cyron <thomas@thcyron.de>
2018-01-12 16:59:03 +0100

    Use %AppDir% for config file path on Windows

---
+++ config_windows.go
@@ -0,0 +1,15 @@
+// +build !windows
+
+package cli
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func init() {
+	dir := os.Getenv("APPDATA")
+	if dir != "" {
+		DefaultConfigPath = filepath.Join(dir, "hcloud", "cli.toml")
+	}
+}
