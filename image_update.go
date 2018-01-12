2923d9c7219f1fa0ab9d6920cbc73a14bc5ff173
Nico Schieder <nico.schieder@hetzner-cloud.de>
2018-01-12 11:01:46 +0100

    Add image update command

---
+++ image_update.go
@@ -0,0 +1,52 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/hetznercloud/hcloud-go/hcloud"
+	"github.com/spf13/cobra"
+)
+
+func newImageUpdateCommand(cli *CLI) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:                   "update [FLAGS] IMAGE",
+		Short:                 "Update an image",
+		Args:                  cobra.ExactArgs(1),
+		TraverseChildren:      true,
+		DisableFlagsInUseLine: true,
+		RunE: cli.wrap(runImageUpdate),
+	}
+
+	cmd.Flags().String("description", "", "Image description")
+
+	cmd.Flags().String("type", "", "Image type")
+	cmd.Flag("type").Annotations = map[string][]string{
+		cobra.BashCompCustom: {"__hcloud_image_types_no_system"},
+	}
+
+	return cmd
+}
+
+func runImageUpdate(cli *CLI, cmd *cobra.Command, args []string) error {
+	idOrName := args[0]
+	image, _, err := cli.Client().Image.Get(cli.Context, idOrName)
+	if err != nil {
+		return err
+	}
+	if image == nil {
+		return fmt.Errorf("image not found: %s", idOrName)
+	}
+
+	description, _ := cmd.Flags().GetString("description")
+	t, _ := cmd.Flags().GetString("type")
+	opts := hcloud.ImageUpdateOpts{
+		Description: hcloud.String(description),
+		Type:        hcloud.ImageType(t),
+	}
+	_, _, err = cli.Client().Image.Update(cli.Context, image, opts)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Image %s updated\n", idOrName)
+	return nil
+}
