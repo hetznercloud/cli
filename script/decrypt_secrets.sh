38f65a59cc5180b7e492bac2c2fd80b91eea3bad
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2020-01-28 14:33:32 +0100

    Use new Cert
    Signed-off-by: Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>

--- script/decrypt_secrets.sh
+++ script/decrypt_secrets.sh
@@ -3,7 +3,7 @@
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSWORD" --output ./.github/secrets/hcloud_cli.p12 ./.github/secrets/hcloud_cli.p12.gpg
 
 security create-keychain -p "" build.keychain
-security import ./.github/secrets/hcloud_cli.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+security import ./.github/secrets/hcloud_cli.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$CERT_PASSWORD" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
