e2bb3673094d06c0dcc76a998633994427ea19f6
minus <minus@mnus.de>
2020-05-11 22:31:19 +0200

    Add command to request a VNC console

---
+++ cli/server_request_console.go
@@ -0,0 +1,45 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func newServerRequestConsoleCommand(cli *CLI) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:                   "request-console [FLAGS] SERVER",
+		Short:                 "Request a WebSocket VNC console for a server",
+		Args:                  cobra.ExactArgs(1),
+		TraverseChildren:      true,
+		DisableFlagsInUseLine: true,
+		PreRunE:               cli.ensureToken,
+		RunE:                  cli.wrap(runServerRequestConsole),
+	}
+	return cmd
+}
+
+func runServerRequestConsole(cli *CLI, cmd *cobra.Command, args []string) error {
+	idOrName := args[0]
+	server, _, err := cli.Client().Server.Get(cli.Context, idOrName)
+	if err != nil {
+		return err
+	}
+	if server == nil {
+		return fmt.Errorf("server not found: %s", idOrName)
+	}
+
+	result, _, err := cli.Client().Server.RequestConsole(cli.Context, server)
+	if err != nil {
+		return err
+	}
+
+	if err := cli.ActionProgress(cli.Context, result.Action); err != nil {
+		return err
+	}
+
+	fmt.Printf("Console for server %d:\n", server.ID)
+	fmt.Printf("WebSocket URL: %s\n", result.WSSURL)
+	fmt.Printf("VNC Password: %s\n", result.Password)
+	return nil
+}
