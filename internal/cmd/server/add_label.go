27b3d38cb00cb2fed2597a0e0d560ba6dd60a41b
Michael Frister <michael.frister@hetzner-cloud.de>
2021-04-29 15:02:13 +0200

    Use generic add label command for servers and images

--- internal/cmd/server/add_label.go
+++ internal/cmd/server/add_label.go
@@ -1,65 +1,33 @@
 package server
 
 import (
+	"context"
 	"fmt"
 
-	"github.com/hetznercloud/cli/internal/cmd/cmpl"
-	"github.com/hetznercloud/cli/internal/cmd/util"
-	"github.com/hetznercloud/cli/internal/state"
+	"github.com/hetznercloud/cli/internal/cmd/base"
+	"github.com/hetznercloud/cli/internal/hcapi2"
 	"github.com/hetznercloud/hcloud-go/hcloud"
-	"github.com/spf13/cobra"
 )
 
-func newAddLabelCommand(cli *state.State) *cobra.Command {
-	cmd := &cobra.Command{
-		Use:                   "add-label [FLAGS] SERVER LABEL",
-		Short:                 "Add a label to a server",
-		Args:                  cobra.ExactArgs(2),
-		ValidArgsFunction:     cmpl.SuggestArgs(cmpl.SuggestCandidatesF(cli.ServerNames)),
-		TraverseChildren:      true,
-		DisableFlagsInUseLine: true,
-		PreRunE:               util.ChainRunE(validateAddLabel, cli.EnsureToken),
-		RunE:                  cli.Wrap(runAddLabel),
-	}
-
-	cmd.Flags().BoolP("overwrite", "o", false, "Overwrite label if it exists already")
-	return cmd
-}
-
-func validateAddLabel(cmd *cobra.Command, args []string) error {
-	label := util.SplitLabel(args[1])
-	if len(label) != 2 {
-		return fmt.Errorf("invalid label: %s", args[1])
-	}
-
-	return nil
-}
-
-func runAddLabel(cli *state.State, cmd *cobra.Command, args []string) error {
-	overwrite, _ := cmd.Flags().GetBool("overwrite")
-	idOrName := args[0]
-	server, _, err := cli.Client().Server.Get(cli.Context, idOrName)
-	if err != nil {
+var addLabelCmd = base.AddLabelCmd{
+	ResourceNameSingular: "server",
+	ShortDescription:     "Add a label to a server",
+	NameSuggestions:      func(c hcapi2.Client) func() []string { return c.Server().Names },
+	FetchLabels: func(ctx context.Context, client hcapi2.Client, idOrName string) (map[string]string, int, error) {
+		server, _, err := client.Server().Get(ctx, idOrName)
+		if err != nil {
+			return nil, 0, err
+		}
+		if server == nil {
+			return nil, 0, fmt.Errorf("server not found: %s", idOrName)
+		}
+		return server.Labels, server.ID, nil
+	},
+	SetLabels: func(ctx context.Context, client hcapi2.Client, id int, labels map[string]string) error {
+		opts := hcloud.ServerUpdateOpts{
+			Labels: labels,
+		}
+		_, _, err := client.Server().Update(ctx, &hcloud.Server{ID: id}, opts)
 		return err
-	}
-	if server == nil {
-		return fmt.Errorf("server not found: %s", idOrName)
-	}
-	label := util.SplitLabel(args[1])
-
-	if _, ok := server.Labels[label[0]]; ok && !overwrite {
-		return fmt.Errorf("label %s on server %d already exists", label[0], server.ID)
-	}
-	labels := server.Labels
-	labels[label[0]] = label[1]
-	opts := hcloud.ServerUpdateOpts{
-		Labels: labels,
-	}
-	_, _, err = cli.Client().Server.Update(cli.Context, server, opts)
-	if err != nil {
-		return err
-	}
-	fmt.Printf("Label %s added to server %d\n", label[0], server.ID)
-
-	return nil
+	},
 }
