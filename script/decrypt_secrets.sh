1d773b17a4ef8029d0129fddc0cf2d0fb61c7370
Julian Tölle <julian.toelle@hetzner-cloud.de>
2022-12-07 11:59:33 +0100

    debug: show keychain info

--- script/decrypt_secrets.sh
+++ script/decrypt_secrets.sh
@@ -9,4 +9,6 @@ security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
 security unlock-keychain -p "" ~/Library/Keychains/build.keychain
 
+security show-keychain-info ~/Library/Keychains/build.keychain
+
 security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
