965882f04b5bfb9fbb73ea2ab3cca263b2cb8082
Julian Tölle <julian.toelle@hetzner-cloud.de>
2022-12-07 13:25:21 +0100

    debug: correct path

--- script/decrypt_secrets.sh
+++ script/decrypt_secrets.sh
@@ -5,7 +5,7 @@ gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSWORD" --output ./
 security create-keychain -p "" build.keychain
 # Use long timeout for keychain to avoid issues where codesign fails because the keychain is locked
 # before it was used. Default timeout is 300s
-security set-keychain-settings -u -t 3600 secrets.keychain
+security set-keychain-settings -u -t 3600 ~/Library/Keychains/build.keychain
 security import ./.github/secrets/hcloud_cli.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$CERT_PASSWORD" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
