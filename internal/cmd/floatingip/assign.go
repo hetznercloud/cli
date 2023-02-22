5c5805a829afad761312b8f93e050d0f055d2595
samcday <me@samcday.com>
2023-02-22 09:57:52 +0100

    chore: replace hcapi.FloatingIPClient usages with hcapi2 (#452)

--- internal/cmd/floatingip/assign.go
+++ internal/cmd/floatingip/assign.go
@@ -1,58 +1,59 @@
 package floatingip
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/hetznercloud/cli/internal/cmd/base"
 	"github.com/hetznercloud/cli/internal/cmd/cmpl"
+	"github.com/hetznercloud/cli/internal/hcapi2"
 	"github.com/hetznercloud/cli/internal/state"
 	"github.com/spf13/cobra"
 )
 
-func newAssignCommand(cli *state.State) *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "assign [FLAGS] FLOATINGIP SERVER",
-		Short: "Assign a Floating IP to a server",
-		Args:  cobra.ExactArgs(2),
-		ValidArgsFunction: cmpl.SuggestArgs(
-			cmpl.SuggestCandidatesF(cli.FloatingIPNames),
-			cmpl.SuggestCandidatesF(cli.ServerNames),
-		),
-		TraverseChildren:      true,
-		DisableFlagsInUseLine: true,
-		PreRunE:               cli.EnsureToken,
-		RunE:                  cli.Wrap(runAssign),
-	}
-	return cmd
-}
+var AssignCommand = base.Cmd{
+	BaseCobraCommand: func(client hcapi2.Client) *cobra.Command {
+		return &cobra.Command{
+			Use:   "assign [FLAGS] FLOATINGIP SERVER",
+			Short: "Assign a Floating IP to a server",
+			Args:  cobra.ExactArgs(2),
+			ValidArgsFunction: cmpl.SuggestArgs(
+				cmpl.SuggestCandidatesF(client.FloatingIP().Names),
+				cmpl.SuggestCandidatesF(client.Server().Names),
+			),
+			TraverseChildren:      true,
+			DisableFlagsInUseLine: true,
+		}
+	},
+	Run: func(ctx context.Context, client hcapi2.Client, waiter state.ActionWaiter, cmd *cobra.Command, args []string) error {
+		idOrName := args[0]
+		floatingIP, _, err := client.FloatingIP().Get(ctx, idOrName)
+		if err != nil {
+			return err
+		}
+		if floatingIP == nil {
+			return fmt.Errorf("Floating IP not found: %v", idOrName)
+		}
 
-func runAssign(cli *state.State, cmd *cobra.Command, args []string) error {
-	idOrName := args[0]
-	floatingIP, _, err := cli.Client().FloatingIP.Get(cli.Context, idOrName)
-	if err != nil {
-		return err
-	}
-	if floatingIP == nil {
-		return fmt.Errorf("Floating IP not found: %v", idOrName)
-	}
+		serverIDOrName := args[1]
+		server, _, err := client.Server().Get(ctx, serverIDOrName)
+		if err != nil {
+			return err
+		}
+		if server == nil {
+			return fmt.Errorf("server not found: %s", serverIDOrName)
+		}
 
-	serverIDOrName := args[1]
-	server, _, err := cli.Client().Server.Get(cli.Context, serverIDOrName)
-	if err != nil {
-		return err
-	}
-	if server == nil {
-		return fmt.Errorf("server not found: %s", serverIDOrName)
-	}
+		action, _, err := client.FloatingIP().Assign(ctx, floatingIP, server)
+		if err != nil {
+			return err
+		}
 
-	action, _, err := cli.Client().FloatingIP.Assign(cli.Context, floatingIP, server)
-	if err != nil {
-		return err
-	}
+		if err := waiter.ActionProgress(ctx, action); err != nil {
+			return err
+		}
 
-	if err := cli.ActionProgress(cli.Context, action); err != nil {
-		return err
-	}
-
-	fmt.Printf("Floating IP %d assigned to server %d\n", floatingIP.ID, server.ID)
-	return nil
+		fmt.Printf("Floating IP %d assigned to server %d\n", floatingIP.ID, server.ID)
+		return nil
+	},
 }
--- internal/cmd/floatingip/unassign.go
+++ internal/cmd/floatingip/unassign.go
@@ -1,46 +1,47 @@
 package floatingip
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/hetznercloud/cli/internal/cmd/base"
 	"github.com/hetznercloud/cli/internal/cmd/cmpl"
+	"github.com/hetznercloud/cli/internal/hcapi2"
 	"github.com/hetznercloud/cli/internal/state"
 	"github.com/spf13/cobra"
 )
 
-func newUnassignCommand(cli *state.State) *cobra.Command {
-	cmd := &cobra.Command{
-		Use:                   "unassign [FLAGS] FLOATINGIP",
-		Short:                 "Unassign a Floating IP",
-		Args:                  cobra.ExactArgs(1),
-		ValidArgsFunction:     cmpl.SuggestArgs(cmpl.SuggestCandidatesF(cli.FloatingIPNames)),
-		TraverseChildren:      true,
-		DisableFlagsInUseLine: true,
-		PreRunE:               cli.EnsureToken,
-		RunE:                  cli.Wrap(runUnassign),
-	}
-	return cmd
-}
+var UnassignCommand = base.Cmd{
+	BaseCobraCommand: func(client hcapi2.Client) *cobra.Command {
+		return &cobra.Command{
+			Use:                   "unassign [FLAGS] FLOATINGIP",
+			Short:                 "Unassign a Floating IP",
+			Args:                  cobra.ExactArgs(1),
+			ValidArgsFunction:     cmpl.SuggestArgs(cmpl.SuggestCandidatesF(client.FloatingIP().Names)),
+			TraverseChildren:      true,
+			DisableFlagsInUseLine: true,
+		}
+	},
+	Run: func(ctx context.Context, client hcapi2.Client, waiter state.ActionWaiter, cmd *cobra.Command, args []string) error {
+		idOrName := args[0]
+		floatingIP, _, err := client.FloatingIP().Get(ctx, idOrName)
+		if err != nil {
+			return err
+		}
+		if floatingIP == nil {
+			return fmt.Errorf("Floating IP not found: %v", idOrName)
+		}
 
-func runUnassign(cli *state.State, cmd *cobra.Command, args []string) error {
-	idOrName := args[0]
-	floatingIP, _, err := cli.Client().FloatingIP.Get(cli.Context, idOrName)
-	if err != nil {
-		return err
-	}
-	if floatingIP == nil {
-		return fmt.Errorf("Floating IP not found: %v", idOrName)
-	}
+		action, _, err := client.FloatingIP().Unassign(ctx, floatingIP)
+		if err != nil {
+			return err
+		}
 
-	action, _, err := cli.Client().FloatingIP.Unassign(cli.Context, floatingIP)
-	if err != nil {
-		return err
-	}
+		if err := waiter.ActionProgress(ctx, action); err != nil {
+			return err
+		}
 
-	if err := cli.ActionProgress(cli.Context, action); err != nil {
-		return err
-	}
-
-	fmt.Printf("Floating IP %d unassigned\n", floatingIP.ID)
-	return nil
+		fmt.Printf("Floating IP %d unassigned\n", floatingIP.ID)
+		return nil
+	},
 }
