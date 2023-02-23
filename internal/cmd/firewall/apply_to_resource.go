7d12ed90b8a7cb86880b9996ed26679bbb4602af
Sam <me@samcday.com>
2023-02-23 09:42:57 +0000

    chore: migrate hcapi.FirewallClient usages to hcapi2

--- internal/cmd/firewall/apply_to_resource.go
+++ internal/cmd/firewall/apply_to_resource.go
@@ -1,95 +1,93 @@
 package firewall
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/hetznercloud/cli/internal/cmd/base"
+	"github.com/hetznercloud/cli/internal/hcapi2"
 	"github.com/hetznercloud/hcloud-go/hcloud"
 	"github.com/spf13/cobra"
 
 	"github.com/hetznercloud/cli/internal/cmd/cmpl"
-	"github.com/hetznercloud/cli/internal/cmd/util"
 	"github.com/hetznercloud/cli/internal/state"
 )
 
-func newApplyToResourceCommand(cli *state.State) *cobra.Command {
-	cmd := &cobra.Command{
-		Use:                   "apply-to-resource FIREWALL FLAGS",
-		Short:                 "Applies a Firewall to a single resource",
-		Args:                  cobra.ExactArgs(1),
-		ValidArgsFunction:     cmpl.SuggestArgs(cmpl.SuggestCandidatesF(cli.FirewallNames)),
-		TraverseChildren:      true,
-		DisableFlagsInUseLine: true,
-		PreRunE:               util.ChainRunE(validateApplyToResource, cli.EnsureToken),
-		RunE:                  cli.Wrap(runApplyToResource),
-	}
-	cmd.Flags().String("type", "", "Resource Type (server) (required)")
-	cmd.RegisterFlagCompletionFunc("type", cmpl.SuggestCandidates("server", "label_selector"))
-	cmd.MarkFlagRequired("type")
-
-	cmd.Flags().String("server", "", "Server name of ID (required when type is server)")
-	cmd.RegisterFlagCompletionFunc("server", cmpl.SuggestCandidatesF(cli.ServerNames))
-
-	cmd.Flags().StringP("label-selector", "l", "", "Label Selector")
-	return cmd
-}
-func validateApplyToResource(cmd *cobra.Command, args []string) error {
-	resourceType, _ := cmd.Flags().GetString("type")
-
-	switch resourceType {
-	case string(hcloud.FirewallResourceTypeServer):
-		server, _ := cmd.Flags().GetString("server")
-		if server == "" {
-			return fmt.Errorf("type %s need a --server specific", resourceType)
+var ApplyToResourceCommand = base.Cmd{
+	BaseCobraCommand: func(client hcapi2.Client) *cobra.Command {
+		cmd := &cobra.Command{
+			Use:                   "apply-to-resource FIREWALL FLAGS",
+			Short:                 "Applies a Firewall to a single resource",
+			Args:                  cobra.ExactArgs(1),
+			ValidArgsFunction:     cmpl.SuggestArgs(cmpl.SuggestCandidatesF(client.Firewall().Names)),
+			TraverseChildren:      true,
+			DisableFlagsInUseLine: true,
 		}
-	case string(hcloud.FirewallResourceTypeLabelSelector):
+		cmd.Flags().String("type", "", "Resource Type (server) (required)")
+		cmd.RegisterFlagCompletionFunc("type", cmpl.SuggestCandidates("server", "label_selector"))
+		cmd.MarkFlagRequired("type")
+
+		cmd.Flags().String("server", "", "Server name of ID (required when type is server)")
+		cmd.RegisterFlagCompletionFunc("server", cmpl.SuggestCandidatesF(client.Server().Names))
+
+		cmd.Flags().StringP("label-selector", "l", "", "Label Selector")
+		return cmd
+	},
+	Run: func(ctx context.Context, client hcapi2.Client, waiter state.ActionWaiter, cmd *cobra.Command, args []string) error {
+		resourceType, _ := cmd.Flags().GetString("type")
+
+		switch resourceType {
+		case string(hcloud.FirewallResourceTypeServer):
+			server, _ := cmd.Flags().GetString("server")
+			if server == "" {
+				return fmt.Errorf("type %s need a --server specific", resourceType)
+			}
+		case string(hcloud.FirewallResourceTypeLabelSelector):
+			labelSelector, _ := cmd.Flags().GetString("label-selector")
+			if labelSelector == "" {
+				return fmt.Errorf("type %s need a --label-selector specific", resourceType)
+			}
+		default:
+			return fmt.Errorf("unknown type %s", resourceType)
+		}
+
+		serverIdOrName, _ := cmd.Flags().GetString("server")
 		labelSelector, _ := cmd.Flags().GetString("label-selector")
-		if labelSelector == "" {
-			return fmt.Errorf("type %s need a --label-selector specific", resourceType)
-		}
-	default:
-		return fmt.Errorf("unknown type %s", resourceType)
-	}
-
-	return nil
-}
-func runApplyToResource(cli *state.State, cmd *cobra.Command, args []string) error {
-	resourceType, _ := cmd.Flags().GetString("type")
-	serverIdOrName, _ := cmd.Flags().GetString("server")
-	labelSelector, _ := cmd.Flags().GetString("label-selector")
-	idOrName := args[0]
-	firewall, _, err := cli.Client().Firewall.Get(cli.Context, idOrName)
-	if err != nil {
-		return err
-	}
-	if firewall == nil {
-		return fmt.Errorf("Firewall not found: %v", idOrName)
-	}
-	opts := hcloud.FirewallResource{Type: hcloud.FirewallResourceType(resourceType)}
-
-	switch opts.Type {
-	case hcloud.FirewallResourceTypeServer:
-		server, _, err := cli.Client().Server.Get(cli.Context, serverIdOrName)
+		idOrName := args[0]
+		firewall, _, err := client.Firewall().Get(ctx, idOrName)
 		if err != nil {
 			return err
 		}
-		if server == nil {
-			return fmt.Errorf("Server not found: %v", serverIdOrName)
+		if firewall == nil {
+			return fmt.Errorf("Firewall not found: %v", idOrName)
 		}
-		opts.Server = &hcloud.FirewallResourceServer{ID: server.ID}
-	case hcloud.FirewallResourceTypeLabelSelector:
-		opts.LabelSelector = &hcloud.FirewallResourceLabelSelector{Selector: labelSelector}
-	default:
-		return fmt.Errorf("unknown type %s", opts.Type)
-	}
+		opts := hcloud.FirewallResource{Type: hcloud.FirewallResourceType(resourceType)}
 
-	actions, _, err := cli.Client().Firewall.ApplyResources(cli.Context, firewall, []hcloud.FirewallResource{opts})
-	if err != nil {
-		return err
-	}
-	if err := cli.ActionsProgresses(cli.Context, actions); err != nil {
-		return err
-	}
-	fmt.Printf("Firewall %d applied\n", firewall.ID)
+		switch opts.Type {
+		case hcloud.FirewallResourceTypeServer:
+			server, _, err := client.Server().Get(ctx, serverIdOrName)
+			if err != nil {
+				return err
+			}
+			if server == nil {
+				return fmt.Errorf("Server not found: %v", serverIdOrName)
+			}
+			opts.Server = &hcloud.FirewallResourceServer{ID: server.ID}
+		case hcloud.FirewallResourceTypeLabelSelector:
+			opts.LabelSelector = &hcloud.FirewallResourceLabelSelector{Selector: labelSelector}
+		default:
+			return fmt.Errorf("unknown type %s", opts.Type)
+		}
 
-	return nil
+		actions, _, err := client.Firewall().ApplyResources(ctx, firewall, []hcloud.FirewallResource{opts})
+		if err != nil {
+			return err
+		}
+		if err := waiter.WaitForActions(ctx, actions); err != nil {
+			return err
+		}
+		fmt.Printf("Firewall %d applied\n", firewall.ID)
+
+		return nil
+	},
 }
--- internal/cmd/firewall/delete_rule.go
+++ internal/cmd/firewall/delete_rule.go
@@ -1,130 +1,131 @@
 package firewall
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"reflect"
 
+	"github.com/hetznercloud/cli/internal/cmd/base"
+	"github.com/hetznercloud/cli/internal/hcapi2"
 	"github.com/hetznercloud/hcloud-go/hcloud"
 	"github.com/spf13/cobra"
 
 	"github.com/hetznercloud/cli/internal/cmd/cmpl"
-	"github.com/hetznercloud/cli/internal/cmd/util"
 	"github.com/hetznercloud/cli/internal/state"
 )
 
-func newDeleteRuleCommand(cli *state.State) *cobra.Command {
-	cmd := &cobra.Command{
-		Use:                   "delete-rule FIREWALL FLAGS",
-		Short:                 "Delete a single rule to a firewall",
-		Args:                  cobra.ExactArgs(1),
-		ValidArgsFunction:     cmpl.SuggestArgs(cmpl.SuggestCandidatesF(cli.FirewallNames)),
-		TraverseChildren:      true,
-		DisableFlagsInUseLine: true,
-		PreRunE:               util.ChainRunE(cli.EnsureToken),
-		RunE:                  cli.Wrap(runDeleteRule),
-	}
-	cmd.Flags().String("direction", "", "Direction (in, out) (required)")
-	cmd.RegisterFlagCompletionFunc("direction", cmpl.SuggestCandidates("in", "out"))
-	cmd.MarkFlagRequired("direction")
-
-	cmd.Flags().String("protocol", "", "Protocol (icmp, esp, gre, udp or tcp) (required)")
-	cmd.RegisterFlagCompletionFunc("protocol", cmpl.SuggestCandidates("icmp", "udp", "tcp", "esp", "gre"))
-	cmd.MarkFlagRequired("protocol")
-
-	cmd.Flags().StringArray("source-ips", []string{}, "Source IPs (CIDR Notation) (required when direction is in)")
-
-	cmd.Flags().StringArray("destination-ips", []string{}, "Destination IPs (CIDR Notation) (required when direction is out)")
-
-	cmd.Flags().String("port", "", "Port to which traffic will be allowed, only applicable for protocols TCP and UDP")
-
-	cmd.Flags().String("description", "", "Description of the firewall rule")
-	return cmd
-}
-
-func runDeleteRule(cli *state.State, cmd *cobra.Command, args []string) error {
-	direction, _ := cmd.Flags().GetString("direction")
-	protocol, _ := cmd.Flags().GetString("protocol")
-	sourceIPs, _ := cmd.Flags().GetStringArray("source-ips")
-	destinationIPs, _ := cmd.Flags().GetStringArray("destination-ips")
-	port, _ := cmd.Flags().GetString("port")
-	description, _ := cmd.Flags().GetString("description")
-
-	idOrName := args[0]
-	firewall, _, err := cli.Client().Firewall.Get(cli.Context, idOrName)
-	if err != nil {
-		return err
-	}
-	if firewall == nil {
-		return fmt.Errorf("Firewall not found: %v", idOrName)
-	}
-
-	d := hcloud.FirewallRuleDirection(direction)
-	rule := hcloud.FirewallRule{
-		Direction: d,
-		Protocol:  hcloud.FirewallRuleProtocol(protocol),
-	}
-	if port != "" {
-		rule.Port = hcloud.String(port)
-	}
-	if description != "" {
-		rule.Description = hcloud.String(description)
-	}
-
-	switch rule.Protocol {
-	case hcloud.FirewallRuleProtocolTCP:
-	case hcloud.FirewallRuleProtocolUDP:
-		if port == "" {
-			return fmt.Errorf("port is required")
+var DeleteRuleCommand = base.Cmd{
+	BaseCobraCommand: func(client hcapi2.Client) *cobra.Command {
+		cmd := &cobra.Command{
+			Use:                   "delete-rule FIREWALL FLAGS",
+			Short:                 "Delete a single rule to a firewall",
+			Args:                  cobra.ExactArgs(1),
+			ValidArgsFunction:     cmpl.SuggestArgs(cmpl.SuggestCandidatesF(client.Firewall().Names)),
+			TraverseChildren:      true,
+			DisableFlagsInUseLine: true,
 		}
-	default:
+		cmd.Flags().String("direction", "", "Direction (in, out) (required)")
+		cmd.RegisterFlagCompletionFunc("direction", cmpl.SuggestCandidates("in", "out"))
+		cmd.MarkFlagRequired("direction")
+
+		cmd.Flags().String("protocol", "", "Protocol (icmp, esp, gre, udp or tcp) (required)")
+		cmd.RegisterFlagCompletionFunc("protocol", cmpl.SuggestCandidates("icmp", "udp", "tcp", "esp", "gre"))
+		cmd.MarkFlagRequired("protocol")
+
+		cmd.Flags().StringArray("source-ips", []string{}, "Source IPs (CIDR Notation) (required when direction is in)")
+
+		cmd.Flags().StringArray("destination-ips", []string{}, "Destination IPs (CIDR Notation) (required when direction is out)")
+
+		cmd.Flags().String("port", "", "Port to which traffic will be allowed, only applicable for protocols TCP and UDP")
+
+		cmd.Flags().String("description", "", "Description of the firewall rule")
+		return cmd
+	},
+	Run: func(ctx context.Context, client hcapi2.Client, waiter state.ActionWaiter, cmd *cobra.Command, args []string) error {
+		direction, _ := cmd.Flags().GetString("direction")
+		protocol, _ := cmd.Flags().GetString("protocol")
+		sourceIPs, _ := cmd.Flags().GetStringArray("source-ips")
+		destinationIPs, _ := cmd.Flags().GetStringArray("destination-ips")
+		port, _ := cmd.Flags().GetString("port")
+		description, _ := cmd.Flags().GetString("description")
+
+		idOrName := args[0]
+		firewall, _, err := client.Firewall().Get(ctx, idOrName)
+		if err != nil {
+			return err
+		}
+		if firewall == nil {
+			return fmt.Errorf("Firewall not found: %v", idOrName)
+		}
+
+		d := hcloud.FirewallRuleDirection(direction)
+		rule := hcloud.FirewallRule{
+			Direction: d,
+			Protocol:  hcloud.FirewallRuleProtocol(protocol),
+		}
 		if port != "" {
-			return fmt.Errorf("port is not allowed for this protocol")
+			rule.Port = hcloud.String(port)
 		}
-	}
+		if description != "" {
+			rule.Description = hcloud.String(description)
+		}
 
-	switch d {
-	case hcloud.FirewallRuleDirectionOut:
-		rule.DestinationIPs = make([]net.IPNet, len(destinationIPs))
-		for i, ip := range destinationIPs {
-			n, err := validateFirewallIP(ip)
-			if err != nil {
-				return fmt.Errorf("destination ips error on index %d: %s", i, err)
+		switch rule.Protocol {
+		case hcloud.FirewallRuleProtocolTCP:
+		case hcloud.FirewallRuleProtocolUDP:
+			if port == "" {
+				return fmt.Errorf("port is required")
 			}
-			rule.DestinationIPs[i] = *n
-			rule.SourceIPs = make([]net.IPNet, 0)
-		}
-	case hcloud.FirewallRuleDirectionIn:
-		rule.SourceIPs = make([]net.IPNet, len(sourceIPs))
-		for i, ip := range sourceIPs {
-			n, err := validateFirewallIP(ip)
-			if err != nil {
-				return fmt.Errorf("source ips error on index %d: %s", i, err)
+		default:
+			if port != "" {
+				return fmt.Errorf("port is not allowed for this protocol")
 			}
-			rule.DestinationIPs = make([]net.IPNet, 0)
-			rule.SourceIPs[i] = *n
 		}
-	}
 
-	var rules []hcloud.FirewallRule
-	for _, existingRule := range firewall.Rules {
-		if !reflect.DeepEqual(existingRule, rule) {
-			rules = append(rules, existingRule)
+		switch d {
+		case hcloud.FirewallRuleDirectionOut:
+			rule.DestinationIPs = make([]net.IPNet, len(destinationIPs))
+			for i, ip := range destinationIPs {
+				n, err := validateFirewallIP(ip)
+				if err != nil {
+					return fmt.Errorf("destination ips error on index %d: %s", i, err)
+				}
+				rule.DestinationIPs[i] = *n
+				rule.SourceIPs = make([]net.IPNet, 0)
+			}
+		case hcloud.FirewallRuleDirectionIn:
+			rule.SourceIPs = make([]net.IPNet, len(sourceIPs))
+			for i, ip := range sourceIPs {
+				n, err := validateFirewallIP(ip)
+				if err != nil {
+					return fmt.Errorf("source ips error on index %d: %s", i, err)
+				}
+				rule.DestinationIPs = make([]net.IPNet, 0)
+				rule.SourceIPs[i] = *n
+			}
 		}
-	}
-	if len(rules) == len(firewall.Rules) {
-		return fmt.Errorf("the specified rule was not found in the ruleset of Firewall %d", firewall.ID)
-	}
-	actions, _, err := cli.Client().Firewall.SetRules(cli.Context, firewall,
-		hcloud.FirewallSetRulesOpts{Rules: rules},
-	)
-	if err != nil {
-		return err
-	}
-	if err := cli.ActionsProgresses(cli.Context, actions); err != nil {
-		return err
-	}
-	fmt.Printf("Firewall Rules for Firewall %d updated\n", firewall.ID)
 
-	return nil
+		var rules []hcloud.FirewallRule
+		for _, existingRule := range firewall.Rules {
+			if !reflect.DeepEqual(existingRule, rule) {
+				rules = append(rules, existingRule)
+			}
+		}
+		if len(rules) == len(firewall.Rules) {
+			return fmt.Errorf("the specified rule was not found in the ruleset of Firewall %d", firewall.ID)
+		}
+		actions, _, err := client.Firewall().SetRules(ctx, firewall,
+			hcloud.FirewallSetRulesOpts{Rules: rules},
+		)
+		if err != nil {
+			return err
+		}
+		if err := waiter.WaitForActions(ctx, actions); err != nil {
+			return err
+		}
+		fmt.Printf("Firewall Rules for Firewall %d updated\n", firewall.ID)
+
+		return nil
+	},
 }
