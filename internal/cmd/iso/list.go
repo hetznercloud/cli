00e43751999d26b34cb7923da2f5349cf06af06f
Julian Tölle <julian.toelle@hetzner-cloud.de>
2023-03-24 15:49:06 +0100

    feat(iso): architecture filter on list command

--- internal/cmd/iso/list.go
+++ internal/cmd/iso/list.go
@@ -2,6 +2,7 @@ package iso
 
 import (
 	"context"
+	"github.com/hetznercloud/cli/internal/cmd/cmpl"
 
 	"github.com/hetznercloud/cli/internal/cmd/base"
 	"github.com/hetznercloud/cli/internal/cmd/output"
@@ -15,12 +16,32 @@ import (
 var listCmd = base.ListCmd{
 	ResourceNamePlural: "isos",
 	DefaultColumns:     []string{"id", "name", "description", "type", "architecture"},
+	AdditionalFlags: func(cmd *cobra.Command) {
+		cmd.Flags().StringSlice("architecture", []string{}, "Only show images of given architecture: x86|arm")
+		cmd.RegisterFlagCompletionFunc("architecture", cmpl.SuggestCandidates(string(hcloud.ArchitectureX86), string(hcloud.ArchitectureARM)))
+
+		cmd.Flags().Bool("include-architecture-wildcard", false, "Include ISOs with unknown architecture, only required if you want so show custom ISOs and still filter for architecture.")
+	},
 
 	Fetch: func(ctx context.Context, client hcapi2.Client, cmd *cobra.Command, listOpts hcloud.ListOpts, sorts []string) ([]interface{}, error) {
 		opts := hcloud.ISOListOpts{ListOpts: listOpts}
+
+		architecture, _ := cmd.Flags().GetStringSlice("architecture")
+		if len(architecture) > 0 {
+			for _, arch := range architecture {
+				opts.Architecture = append(opts.Architecture, hcloud.Architecture(arch))
+			}
+		}
+
+		includeArchitectureWildcard, _ := cmd.Flags().GetBool("include-architecture-wildcard")
+		if includeArchitectureWildcard {
+			opts.IncludeWildcardArchitecture = includeArchitectureWildcard
+		}
+
 		if len(sorts) > 0 {
 			opts.Sort = sorts
 		}
+
 		isos, _, err := client.ISO().List(ctx, opts)
 
 		var resources []interface{}
