01edc6790ccff80719735a25384587add76b037b
Julian Tölle <julian.toelle@hetzner-cloud.de>
2023-03-28 16:00:27 +0200

    feat(image): architecture filter on describe command

--- internal/cmd/image/describe.go
+++ internal/cmd/image/describe.go
@@ -3,9 +3,11 @@ package image
 import (
 	"context"
 	"fmt"
+	"os"
 
 	humanize "github.com/dustin/go-humanize"
 	"github.com/hetznercloud/cli/internal/cmd/base"
+	"github.com/hetznercloud/cli/internal/cmd/cmpl"
 	"github.com/hetznercloud/cli/internal/cmd/util"
 	"github.com/hetznercloud/cli/internal/hcapi2"
 	"github.com/hetznercloud/hcloud-go/hcloud"
@@ -17,9 +19,21 @@ var describeCmd = base.DescribeCmd{
 	ShortDescription:     "Describe an image",
 	JSONKeyGetByID:       "image",
 	JSONKeyGetByName:     "images",
-	NameSuggestions:      func(c hcapi2.Client) func() []string { return c.Image().Names },
+	AdditionalFlags: func(cmd *cobra.Command) {
+		cmd.Flags().StringP("architecture", "a", string(hcloud.ArchitectureX86), "architecture of the image, default is x86")
+		cmd.RegisterFlagCompletionFunc("architecture", cmpl.SuggestCandidates(string(hcloud.ArchitectureX86), string(hcloud.ArchitectureARM)))
+
+	},
+	NameSuggestions: func(c hcapi2.Client) func() []string { return c.Image().Names },
 	Fetch: func(ctx context.Context, client hcapi2.Client, cmd *cobra.Command, idOrName string) (interface{}, *hcloud.Response, error) {
-		return client.Image().Get(ctx, idOrName)
+		arch, err := cmd.Flags().GetString("architecture")
+		if err != nil {
+			return nil, nil, err
+		}
+		if !cmd.Flags().Changed("architecture") {
+			_, _ = fmt.Fprintln(os.Stderr, "INFO: This command only returns x86 images by default. Explicitly set the --architecture=x86|arm flag to hide this message.")
+		}
+		return client.Image().GetForArchitecture(ctx, idOrName, hcloud.Architecture(arch))
 	},
 	PrintText: func(_ context.Context, _ hcapi2.Client, _ *cobra.Command, resource interface{}) error {
 		image := resource.(*hcloud.Image)
