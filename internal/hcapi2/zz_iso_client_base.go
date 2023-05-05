c70c34be87be6d4b222dd336e6690b86faf651e2
Markus Freitag <fmarkus@mailbox.org>
2023-05-05 16:24:15 +0200

    feat(iso): fix list subcommand
    Previously ISOClient.List() has been used, which results in fetching
    the first page only. Replacing it with ISOClient.AllWithOpts introduces
    auto-pagination handling.

--- internal/hcapi2/zz_iso_client_base.go
+++ internal/hcapi2/zz_iso_client_base.go
@@ -10,6 +10,7 @@ import (
 // ISOClientBase is an interface generated for "github.com/hetznercloud/hcloud-go/hcloud.ISOClient".
 type ISOClientBase interface {
 	All(context.Context) ([]*hcloud.ISO, error)
+	AllWithOpts(context.Context, hcloud.ISOListOpts) ([]*hcloud.ISO, error)
 	Get(context.Context, string) (*hcloud.ISO, *hcloud.Response, error)
 	GetByID(context.Context, int) (*hcloud.ISO, *hcloud.Response, error)
 	GetByName(context.Context, string) (*hcloud.ISO, *hcloud.Response, error)
