57748b733492a03e7c14f3a1fdea9d74921092ce
Marvin A. Ruder <signed@mruder.dev>
2023-08-17 10:56:08 +0200

    Document supported volume formats
    * Add to flag description
    * Add as shell completion
    
    Signed-off-by: Marvin A. Ruder <signed@mruder.dev>

--- internal/cmd/volume/create.go
+++ internal/cmd/volume/create.go
@@ -35,7 +35,9 @@ var CreateCommand = base.Cmd{
 		cmd.MarkFlagRequired("size")
 
 		cmd.Flags().Bool("automount", false, "Automount volume after attach (server must be provided)")
-		cmd.Flags().String("format", "", "Format volume after creation")
+
+		cmd.Flags().String("format", "", "Format volume after creation (ext4 or xfs)")
+		cmd.RegisterFlagCompletionFunc("format", cmpl.SuggestCandidates("ext4", "xfs"))
 
 		cmd.Flags().StringToString("label", nil, "User-defined labels ('key=value') (can be specified multiple times)")
 
