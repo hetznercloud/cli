hetznercloud/cli

commit 54e582a7253274ef5b578b01fc9f992ef6eae226

feat(context): ask to use HCLOUD_TOKEN when creating new context (#582)
Closes #576

internal/cmd/context/create.go
@@ -1,14 +1,16 @@
 package context
 
 import (
+	"bufio"
 	"bytes"
 	"errors"
 	"fmt"
+	"os"
 	"strings"
 	"syscall"
 
 	"github.com/spf13/cobra"
-	"golang.org/x/crypto/ssh/terminal"
+	"golang.org/x/term"
 
 	"github.com/hetznercloud/cli/internal/state"
 )
@@ -40,24 +42,43 @@ func runCreate(cli *state.State, _ *cobra.Command, args []string) error {
 
 	context := &state.ConfigContext{Name: name}
 
-	for {
-		fmt.Printf("Token: ")
-		btoken, err := terminal.ReadPassword(int(syscall.Stdin))
-		fmt.Print("\n")
-		if err != nil {
-			return err
+	var token string
+
+	envToken := os.Getenv("HCLOUD_TOKEN")
+	if envToken != "" {
+		if len(envToken) != 64 {
+			fmt.Println("Warning: HCLOUD_TOKEN is set, but token is invalid (must be exactly 64 characters long)")
+		} else {
+			fmt.Print("The HCLOUD_TOKEN environment variable is set. Do you want to use the token from HCLOUD_TOKEN for the new context? (Y/n): ")
+			scanner := bufio.NewScanner(os.Stdin)
+			scanner.Scan()
+			if s := strings.ToLower(scanner.Text()); s == "" || s == "y" || s == "yes" {
+				token = envToken
+			}
 		}
-		token := string(bytes.TrimSpace(btoken))
-		if token == "" {
-			continue
-		}
-		if len(token) != 64 {
-			fmt.Print("Entered token is invalid (must be exactly 64 characters long)\n")
-			continue
-		}
-		context.Token = token
-		break
 	}
+
+	if token == "" {
+		for {
+			fmt.Printf("Token: ")
+			btoken, err := term.ReadPassword(syscall.Stdin)
+			fmt.Print("\n")
+			if err != nil {
+				return err
+			}
+			token = string(bytes.TrimSpace(btoken))
+			if token == "" {
+				continue
+			}
+			if len(token) != 64 {
+				fmt.Print("Entered token is invalid (must be exactly 64 characters long)\n")
+				continue
+			}
+			break
+		}
+	}
+
+	context.Token = token
 
 	cli.Config.Contexts = append(cli.Config.Contexts, context)
 	cli.Config.ActiveContext = context

commit 63ca6decf95f3cdaf48e3a7110eadbe6c851710a

chore: convert syscall.Stdin to int to fix build on Windows (#584)
This fixes a bug that was introduced in #582 
Example pipeline in which the bug occured:
https://github.com/hetznercloud/cli/actions/runs/6638017209/job/18033507784#step:4:57

internal/cmd/context/create.go
@@ -61,7 +61,9 @@ func runCreate(cli *state.State, _ *cobra.Command, args []string) error {
 	if token == "" {
 		for {
 			fmt.Printf("Token: ")
-			btoken, err := term.ReadPassword(syscall.Stdin)
+			// Conversion needed for compilation on Windows
+			//                               vvv
+			btoken, err := term.ReadPassword(int(syscall.Stdin))
 			fmt.Print("\n")
 			if err != nil {
 				return err