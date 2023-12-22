af9dec448dfaf5331d5200d96722e5c648c25624
pauhull <pauhull3@gmail.com>
2023-12-22 16:33:13 +0100

    ci: assign version using release-please (#654)
    This PR changes version number output to the following:
    
    ### Release
    ```
    $ hcloud version
    hcloud 1.23.4
    ```
    
    ### goreleaser --snapshot
    ```
    $ hcloud version
    hcloud 1.23.4-dev+<short commit id>
    ```
    
    ### go build/go install
    ```
    $ hcloud version
    hcloud 1.23.4-dev
    ```
    
    Closes #636
    
    ---------
    
    Co-authored-by: jo <ljonas@riseup.net>

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,4 +1,19 @@
 package version
 
-// Version is set via compiler flags (see goreleaser config)
-var Version = "unknown"
+var (
+	// version is a semver version (https://semver.org).
+	version = "1.41.1" // x-release-please-version
+
+	// versionPrerelease is a semver version pre-release identifier (https://semver.org).
+	//
+	// For final releases, we set this to an empty string.
+	versionPrerelease = "dev"
+
+	// Version of the hcloud CLI.
+	Version = func() string {
+		if versionPrerelease != "" {
+			return version + "-" + versionPrerelease
+		}
+		return version
+	}()
+)
