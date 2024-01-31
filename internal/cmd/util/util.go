af546fe55783a2b9c83775a7b598d4d1221e4657
pauhull <22707808+pauhull@users.noreply.github.com>
2024-01-31 14:26:14 +0100

    add colons to command group titles

--- internal/cmd/util/util.go
+++ internal/cmd/util/util.go
@@ -195,6 +195,9 @@ func ValidateRequiredFlags(flags *pflag.FlagSet, names ...string) error {
 
 // AddGroup adds a group to the passed command and adds the passed commands to the group.
 func AddGroup(cmd *cobra.Command, id string, title string, groupCmds ...*cobra.Command) {
+	if !strings.HasSuffix(title, ":") {
+		title += ":"
+	}
 	cmd.AddGroup(&cobra.Group{ID: id, Title: title})
 	for _, groupCmd := range groupCmds {
 		groupCmd.GroupID = id
