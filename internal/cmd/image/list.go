aade48bf904862472bc349ee88c1be81fb0ca0f0
kranurag7 <anurag.kumar@syself.com>
2024-02-07 03:47:08 +0530

    add available types to hcloud image list
    Signed-off-by: kranurag7 <anurag.kumar@syself.com>
    
    Co-authored-by: Julian Tölle <julian.toelle97@gmail.com>

--- internal/cmd/image/list.go
+++ internal/cmd/image/list.go
@@ -24,7 +24,7 @@ var ListCmd = base.ListCmd{
 	JSONKeyGetByName:   "images",
 	DefaultColumns:     []string{"id", "type", "name", "description", "architecture", "image_size", "disk_size", "created", "deprecated"},
 	AdditionalFlags: func(cmd *cobra.Command) {
-		cmd.Flags().StringSliceP("type", "t", []string{}, "Only show images of given type")
+		cmd.Flags().StringSliceP("type", "t", []string{}, "Only show images of given type: system|app|snapshot|backup")
 		cmd.RegisterFlagCompletionFunc("type", cmpl.SuggestCandidates("backup", "snapshot", "system", "app"))
 
 		cmd.Flags().StringSliceP("architecture", "a", []string{}, "Only show images of given architecture: x86|arm")
