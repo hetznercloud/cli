eb768892bca8b708d15ab0ab431115a8f22afe15
pauhull <22707808+phm07@users.noreply.github.com>
2024-03-20 13:16:42 +0100

    feat: allow deletion of multiple resources at once

--- internal/cmd/base/delete.go
+++ internal/cmd/base/delete.go
@@ -1,6 +1,7 @@
 package base
 
 import (
+	"errors"
 	"fmt"
 	"reflect"
 	"strings"
@@ -27,9 +28,9 @@ type DeleteCmd struct {
 // CobraCommand creates a command that can be registered with cobra.
 func (dc *DeleteCmd) CobraCommand(s state.State) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:                   fmt.Sprintf("delete [FLAGS] %s", strings.ToUpper(dc.ResourceNameSingular)),
+		Use:                   fmt.Sprintf("delete [FLAGS] %s...", strings.ToUpper(dc.ResourceNameSingular)),
 		Short:                 dc.ShortDescription,
-		Args:                  cobra.ExactArgs(1),
+		Args:                  cobra.MinimumNArgs(1),
 		ValidArgsFunction:     cmpl.SuggestArgs(cmpl.SuggestCandidatesF(dc.NameSuggestions(s.Client()))),
 		TraverseChildren:      true,
 		DisableFlagsInUseLine: true,
@@ -46,22 +47,27 @@ func (dc *DeleteCmd) CobraCommand(s state.State) *cobra.Command {
 
 // Run executes a describe command.
 func (dc *DeleteCmd) Run(s state.State, cmd *cobra.Command, args []string) error {
+	var cmdErr error
 
-	idOrName := args[0]
-	resource, _, err := dc.Fetch(s, cmd, idOrName)
-	if err != nil {
-		return err
+	for _, idOrName := range args {
+		resource, _, err := dc.Fetch(s, cmd, idOrName)
+		if err != nil {
+			cmdErr = errors.Join(cmdErr, err)
+			continue
+		}
+
+		// resource is an interface that always has a type, so the interface is never nil
+		// (i.e. == nil) is always false.
+		if reflect.ValueOf(resource).IsNil() {
+			cmdErr = errors.Join(cmdErr, fmt.Errorf("%s not found: %s", dc.ResourceNameSingular, idOrName))
+			continue
+		}
+
+		if err = dc.Delete(s, cmd, resource); err != nil {
+			cmdErr = errors.Join(cmdErr, fmt.Errorf("deleting %s %s failed: %s", dc.ResourceNameSingular, idOrName, err))
+		}
+		cmd.Printf("%s %v deleted\n", dc.ResourceNameSingular, idOrName)
 	}
 
-	// resource is an interface that always has a type, so the interface is never nil
-	// (i.e. == nil) is always false.
-	if reflect.ValueOf(resource).IsNil() {
-		return fmt.Errorf("%s not found: %s", dc.ResourceNameSingular, idOrName)
-	}
-
-	if err := dc.Delete(s, cmd, resource); err != nil {
-		return fmt.Errorf("deleting %s %s failed: %s", dc.ResourceNameSingular, idOrName, err)
-	}
-	cmd.Printf("%s %v deleted\n", dc.ResourceNameSingular, idOrName)
-	return nil
+	return cmdErr
 }
