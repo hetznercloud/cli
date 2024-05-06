65219e9db8149d98a50971f0d36a608d46e2e060
jo <ljonas@riseup.net>
2024-05-06 16:43:39 +0200

    chore: generate mocks

--- internal/hcapi2/mock/zz_action_client_mock.go
+++ internal/hcapi2/mock/zz_action_client_mock.go
@@ -97,6 +97,44 @@ func (mr *MockActionClientMockRecorder) List(arg0, arg1 interface{}) *gomock.Cal
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "List", reflect.TypeOf((*MockActionClient)(nil).List), arg0, arg1)
 }
 
+// WaitFor mocks base method.
+func (m *MockActionClient) WaitFor(arg0 context.Context, arg1 ...*hcloud.Action) error {
+	m.ctrl.T.Helper()
+	varargs := []interface{}{arg0}
+	for _, a := range arg1 {
+		varargs = append(varargs, a)
+	}
+	ret := m.ctrl.Call(m, "WaitFor", varargs...)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// WaitFor indicates an expected call of WaitFor.
+func (mr *MockActionClientMockRecorder) WaitFor(arg0 interface{}, arg1 ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	varargs := append([]interface{}{arg0}, arg1...)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "WaitFor", reflect.TypeOf((*MockActionClient)(nil).WaitFor), varargs...)
+}
+
+// WaitForFunc mocks base method.
+func (m *MockActionClient) WaitForFunc(arg0 context.Context, arg1 func(*hcloud.Action) error, arg2 ...*hcloud.Action) error {
+	m.ctrl.T.Helper()
+	varargs := []interface{}{arg0, arg1}
+	for _, a := range arg2 {
+		varargs = append(varargs, a)
+	}
+	ret := m.ctrl.Call(m, "WaitForFunc", varargs...)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// WaitForFunc indicates an expected call of WaitForFunc.
+func (mr *MockActionClientMockRecorder) WaitForFunc(arg0, arg1 interface{}, arg2 ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	varargs := append([]interface{}{arg0, arg1}, arg2...)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "WaitForFunc", reflect.TypeOf((*MockActionClient)(nil).WaitForFunc), varargs...)
+}
+
 // WatchOverallProgress mocks base method.
 func (m *MockActionClient) WatchOverallProgress(arg0 context.Context, arg1 []*hcloud.Action) (<-chan int, <-chan error) {
 	m.ctrl.T.Helper()
