763fa5f28f9dcc11b4a0caff25499d7ea54dda21
phm07 <22707808+phm07@users.noreply.github.com>
2024-05-29 13:49:51 +0200

    add tests for preference validation

--- internal/state/config/preferences_test.go
+++ internal/state/config/preferences_test.go
@@ -156,6 +156,14 @@ func TestPreferences_Validate(t *testing.T) {
 		assert.NoError(t, p.Validate())
 	})
 
+	t.Run("existing nested", func(t *testing.T) {
+		_, cleanup := config.NewTestOption("foo.bar", "", "", config.OptionFlagPreference, nil)
+		defer cleanup()
+
+		p := config.Preferences{"foo": map[string]any{"bar": ""}}
+		assert.NoError(t, p.Validate())
+	})
+
 	t.Run("existing but no preference", func(t *testing.T) {
 		_, cleanup := config.NewTestOption("foo", "", "", 0, nil)
 		defer cleanup()
@@ -164,8 +172,37 @@ func TestPreferences_Validate(t *testing.T) {
 		assert.EqualError(t, p.Validate(), "unknown preference: foo")
 	})
 
+	t.Run("existing nested but no preference", func(t *testing.T) {
+		_, cleanup := config.NewTestOption("foo.bar", "", "", 0, nil)
+		defer cleanup()
+
+		p := config.Preferences{"foo": map[string]any{"bar": ""}}
+		assert.EqualError(t, p.Validate(), "unknown preference: foo.bar")
+	})
+
 	t.Run("not existing", func(t *testing.T) {
 		p := config.Preferences{"foo": ""}
 		assert.EqualError(t, p.Validate(), "unknown preference: foo")
+	})
+
+	t.Run("not existing nested", func(t *testing.T) {
+		p := config.Preferences{"foo": map[string]any{"bar": ""}}
+		assert.EqualError(t, p.Validate(), "unknown preference: foo.bar")
+	})
+
+	t.Run("not existing deeply nested", func(t *testing.T) {
+		_, cleanup := config.NewTestOption("foo.bar", "", "", 0, nil)
+		defer cleanup()
+
+		p := config.Preferences{"foo": map[string]any{"bar": map[string]any{"baz": ""}}}
+		assert.EqualError(t, p.Validate(), "unknown preference: foo.bar.baz")
+	})
+
+	t.Run("nested missing map", func(t *testing.T) {
+		_, cleanup := config.NewTestOption("foo.bar.baz", "", "", 0, nil)
+		defer cleanup()
+
+		p := config.Preferences{"foo": map[string]any{"bar": "this should be a map"}}
+		assert.EqualError(t, p.Validate(), "unknown preference: foo.bar")
 	})
 }
