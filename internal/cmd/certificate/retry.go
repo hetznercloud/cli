e46419c9519947dc3e7e2839fee5f1619f41c84a
phm07 <22707808+phm07@users.noreply.github.com>
2024-08-26 10:07:12 +0200

    chore: fix build error in 0223f7d

--- internal/cmd/certificate/retry.go
+++ internal/cmd/certificate/retry.go
@@ -34,7 +34,7 @@ var RetryCmd = base.Cmd{
 			return err
 		}
 
-		if err := s.WaitForActions(cmd, s, action); err != nil {
+		if err := s.WaitForActions(s, cmd, action); err != nil {
 			return err
 		}
 
