41565444ae3044f90efe3088641f1923485e4fd7
phm07 <22707808+phm07@users.noreply.github.com>
2024-10-09 13:41:31 +0200

    test(e2e): add back check for uniqueness_error in network test (#879)
    The underlying issue in the API was fixed and we can now add the test
    back again.

--- test/e2e/network_test.go
+++ test/e2e/network_test.go
@@ -28,8 +28,7 @@ func TestNetwork(t *testing.T) {
 
 	_, err = createNetwork(t, networkName, "--ip-range", "10.0.1.0/24")
 	require.Error(t, err)
-	// TODO: API currently returns service_error instead of uniqueness_error. Add this back in once this is fixed.
-	// assert.Regexp(t, `^name is already used \(uniqueness_error, [0-9a-f]+\)$`, err.Error())
+	assert.Regexp(t, `^name is already used \(uniqueness_error, [0-9a-f]+\)$`, err.Error())
 
 	t.Run("enable-protection", func(t *testing.T) {
 		t.Run("non-existing-protection", func(t *testing.T) {
