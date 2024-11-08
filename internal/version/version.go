c49a7ec4288433ba27ef1322719f277a4960fd51
Julian Tölle <julian.toelle@hetzner-cloud.de>
2024-11-08 13:12:34 +0100

    ci: switch to releaser-pleaser (#906)
    Switch to releaser-pleaser[0] for planning new releases.
    This adds the following new features:
    
    - We can easily cut pre-releases, these are always useful if we want to
    allow some customers to test features before making an official release
    or to validate changes made to the release CI workflows
    - We can add our own release notes to each release to better inform
    customers about the new features we added or about any deprecations.
    
    [0] https://github.com/apricote/releaser-pleaser

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// version is a semver version (https://semver.org).
-	version = "1.48.0" // x-release-please-version
+	version = "1.48.0" // x-releaser-pleaser-version
 
 	// versionPrerelease is a semver version pre-release identifier (https://semver.org).
 	//
