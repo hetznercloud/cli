2bed829fa12914ee46aeb7ab6754d0b566d36483
Jonas L. <jooola@users.noreply.github.com>
2024-11-08 11:44:13 +0100

    chore: move tools to module to prevent bloating the root module (#901)
    This dedicated module is used to prevent bloating the root Go module.
    
    The mock package is still in the root module, as it is not an external
    tool, but is part of the code.

---
+++ tools/tools.go
@@ -0,0 +1,9 @@
+package tools
+
+// This dedicated Go module is used to prevent bloating the root Go module with external
+// tools.
+
+import (
+	_ "github.com/anchore/quill/cmd/quill"
+	_ "github.com/boumenot/gocover-cobertura"
+)
