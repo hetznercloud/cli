b2223ab6a0177b958f9c162bb3bf64224b104809
Julian Tölle <julian.toelle@hetzner-cloud.de>
2025-08-20 11:29:13 +0200

    feat: add storage-box-type command (#1131)
    Adds the CRUD commands `hcloud storage-box-type describe` and `hcloud
    storage-box-type list`.
    
    Related to #1117
    
    ---------
    
    Co-authored-by: lukasmetzner <lukas.metzner@hetzner-cloud.de>

--- docs/reference/manual/hcloud.md
+++ docs/reference/manual/hcloud.md
@@ -42,6 +42,7 @@ A command-line interface for Hetzner Cloud
 * [hcloud server](hcloud_server.md)	 - Manage Servers
 * [hcloud server-type](hcloud_server-type.md)	 - View Server Types
 * [hcloud ssh-key](hcloud_ssh-key.md)	 - Manage SSH Keys
+* [hcloud storage-box-type](hcloud_storage-box-type.md)	 - View Storage Box Types
 * [hcloud version](hcloud_version.md)	 - Print version information
 * [hcloud volume](hcloud_volume.md)	 - Manage Volumes
 * [hcloud zone](hcloud_zone.md)	 - [experimental] Manage DNS Zones and Zone RRSets (records)
---
+++ internal/cmd/storageboxtype/describe_test.go
@@ -0,0 +1,98 @@
+package storageboxtype_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+
+	"github.com/hetznercloud/cli/internal/cmd/storageboxtype"
+	"github.com/hetznercloud/cli/internal/testutil"
+	"github.com/hetznercloud/hcloud-go/v2/hcloud"
+)
+
+func TestDescribe(t *testing.T) {
+	fx := testutil.NewFixture(t)
+	defer fx.Finish()
+
+	cmd := storageboxtype.DescribeCmd.CobraCommand(fx.State())
+	fx.ExpectEnsureToken()
+
+	fx.Client.StorageBoxTypeClient.EXPECT().
+		Get(gomock.Any(), "bx11").
+		Return(&hcloud.StorageBoxType{
+			ID:                     42,
+			Name:                   "bx11",
+			Description:            "BX11",
+			SnapshotLimit:          10,
+			AutomaticSnapshotLimit: 10,
+			SubaccountsLimit:       200,
+			Size:                   1073741824,
+			Pricings: []hcloud.StorageBoxTypeLocationPricing{
+				{
+					Location: "fsn1",
+					PriceHourly: hcloud.Price{
+						Gross: "0.0051",
+						Net:   "0.0051",
+					},
+					PriceMonthly: hcloud.Price{
+						Gross: "3.2000",
+						Net:   "3.2000",
+					},
+					SetupFee: hcloud.Price{
+						Gross: "0.0000",
+						Net:   "0.0000",
+					},
+				},
+				{
+					Location: "hel1",
+					PriceHourly: hcloud.Price{
+						Gross: "0.0051",
+						Net:   "0.0051",
+					},
+					PriceMonthly: hcloud.Price{
+						Gross: "3.2000",
+						Net:   "3.2000",
+					},
+					SetupFee: hcloud.Price{
+						Gross: "0.0000",
+						Net:   "0.0000",
+					},
+				},
+			},
+		}, nil, nil)
+
+	fx.Client.PricingClient.EXPECT().
+		Get(gomock.Any()).
+		Return(hcloud.Pricing{
+			Currency: "EUR",
+			VATRate:  "1.19",
+		}, nil, nil)
+
+	out, errOut, err := fx.Run(cmd, []string{"bx11"})
+
+	expOut := `ID:				42
+Name:				bx11
+Description:			BX11
+Size:				1.0 GiB
+Snapshot Limit:			10
+Automatic Snapshot Limit:	10
+Subaccounts Limit:		200
+Pricings per Location:
+  - Location:	fsn1
+    Hourly:	€ 0.0051
+    Monthly:	€ 3.2000
+    Setup Fee:	€ 0.0000
+
+  - Location:	hel1
+    Hourly:	€ 0.0051
+    Monthly:	€ 3.2000
+    Setup Fee:	€ 0.0000
+
+`
+
+	require.NoError(t, err)
+	assert.Empty(t, errOut)
+	assert.Equal(t, expOut, out)
+}
