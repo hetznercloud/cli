af002d04ae98ffc1032cecff3fd89ea487739d69
phm07 <22707808+phm07@users.noreply.github.com>
2025-09-03 17:22:07 +0200

    add yaml + json support for listing folders

--- internal/cmd/storagebox/folders.go
+++ internal/cmd/storagebox/folders.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/hetznercloud/cli/internal/cmd/base"
 	"github.com/hetznercloud/cli/internal/cmd/cmpl"
+	"github.com/hetznercloud/cli/internal/cmd/output"
+	"github.com/hetznercloud/cli/internal/cmd/util"
 	"github.com/hetznercloud/cli/internal/hcapi2"
 	"github.com/hetznercloud/cli/internal/state"
 	"github.com/hetznercloud/hcloud-go/v2/hcloud"
@@ -22,11 +24,14 @@ var FoldersCmd = base.Cmd{
 			DisableFlagsInUseLine: true,
 		}
 		cmd.Flags().String("path", "", "Relative path for which the listing is to be made")
+
+		output.AddFlag(cmd, output.OptionJSON(), output.OptionYAML())
 		return cmd
 	},
 	Run: func(s state.State, cmd *cobra.Command, args []string) error {
 		idOrName := args[0]
 		path, _ := cmd.Flags().GetString("path")
+		outOpts := output.FlagsForCommand(cmd)
 
 		storageBox, _, err := s.Client().StorageBox().Get(s, idOrName)
 		if err != nil {
@@ -44,6 +49,19 @@ var FoldersCmd = base.Cmd{
 		result, _, err := s.Client().StorageBox().Folders(s, storageBox, opts)
 		if err != nil {
 			return err
+		}
+
+		if outOpts.IsSet("json") || outOpts.IsSet("yaml") {
+			schema := struct {
+				Folders []string `json:"folders"`
+			}{
+				Folders: result.Folders,
+			}
+
+			if outOpts.IsSet("json") {
+				return util.DescribeJSON(cmd.OutOrStdout(), schema)
+			}
+			return util.DescribeYAML(cmd.OutOrStdout(), schema)
 		}
 
 		if len(result.Folders) == 0 {
--- internal/cmd/storagebox/folders_test.go
+++ internal/cmd/storagebox/folders_test.go
@@ -38,3 +38,54 @@ func TestFolders(t *testing.T) {
 - folder2
 `, out)
 }
+
+func TestFoldersJSON(t *testing.T) {
+	fx := testutil.NewFixture(t)
+	defer fx.Finish()
+
+	cmd := storagebox.FoldersCmd.CobraCommand(fx.State())
+	fx.ExpectEnsureToken()
+
+	fx.Client.StorageBoxClient.EXPECT().
+		Get(gomock.Any(), "test").
+		Return(&hcloud.StorageBox{ID: 123}, nil, nil)
+	fx.Client.StorageBoxClient.EXPECT().
+		Folders(gomock.Any(), &hcloud.StorageBox{ID: 123}, hcloud.StorageBoxFoldersOpts{Path: ""}).
+		Return(hcloud.StorageBoxFoldersResult{
+			Folders: []string{"folder1", "folder2"},
+		}, nil, nil)
+
+	args := []string{"test", "-o=json"}
+	jsonOut, out, err := fx.Run(cmd, args)
+
+	require.NoError(t, err)
+	assert.Empty(t, out)
+	assert.JSONEq(t, `{"folders": ["folder1", "folder2"]}`, jsonOut)
+}
+
+func TestFoldersYAML(t *testing.T) {
+	fx := testutil.NewFixture(t)
+	defer fx.Finish()
+
+	cmd := storagebox.FoldersCmd.CobraCommand(fx.State())
+	fx.ExpectEnsureToken()
+
+	fx.Client.StorageBoxClient.EXPECT().
+		Get(gomock.Any(), "test").
+		Return(&hcloud.StorageBox{ID: 123}, nil, nil)
+	fx.Client.StorageBoxClient.EXPECT().
+		Folders(gomock.Any(), &hcloud.StorageBox{ID: 123}, hcloud.StorageBoxFoldersOpts{Path: ""}).
+		Return(hcloud.StorageBoxFoldersResult{
+			Folders: []string{"folder1", "folder2"},
+		}, nil, nil)
+
+	args := []string{"test", "-o=yaml"}
+	yamlOut, out, err := fx.Run(cmd, args)
+
+	require.NoError(t, err)
+	assert.Empty(t, out)
+	assert.YAMLEq(t, `folders:
+- folder1
+- folder2
+`, yamlOut)
+}
