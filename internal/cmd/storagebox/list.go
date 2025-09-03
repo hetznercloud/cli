1cfb19c96391b3820bfce72e1f3116edec0ed5f9
phm07 <22707808+phm07@users.noreply.github.com>
2025-09-03 12:50:56 +0200

    list command + test

--- internal/cmd/storagebox/list.go
+++ internal/cmd/storagebox/list.go
@@ -1,22 +1,26 @@
 package storagebox
 
 import (
-	"strconv"
+	"time"
+
+	"github.com/dustin/go-humanize"
+	"github.com/spf13/pflag"
 
 	"github.com/hetznercloud/cli/internal/cmd/base"
 	"github.com/hetznercloud/cli/internal/cmd/output"
+	"github.com/hetznercloud/cli/internal/cmd/util"
 	"github.com/hetznercloud/cli/internal/hcapi2"
 	"github.com/hetznercloud/cli/internal/state"
 	"github.com/hetznercloud/hcloud-go/v2/hcloud"
-	"github.com/spf13/pflag"
 )
 
 var ListCmd = base.ListCmd{
 	ResourceNamePlural: "Storage Boxes",
 	JSONKeyGetByName:   "storage_boxes",
-	DefaultColumns:     []string{"id", "name", "type", "username", "size", "location"},
+	DefaultColumns:     []string{"id", "name", "username", "server", "type", "size", "location", "age"},
 	Fetch: func(s state.State, set *pflag.FlagSet, opts hcloud.ListOpts, strings []string) ([]interface{}, error) {
-		storageBoxes, err := s.Client().StorageBox().All(s)
+		listOpts := hcloud.StorageBoxListOpts{ListOpts: opts}
+		storageBoxes, err := s.Client().StorageBox().AllWithOpts(s, listOpts)
 
 		var resources []interface{}
 		for _, r := range storageBoxes {
@@ -27,24 +31,53 @@ var ListCmd = base.ListCmd{
 	OutputTable: func(t *output.Table, client hcapi2.Client) {
 		t.
 			AddAllowedFields(hcloud.StorageBox{}).
-			AddFieldFn("type", output.FieldFn(func(obj interface{}) string {
+			AddFieldFn("type", func(obj any) string {
 				storageBox := obj.(*hcloud.StorageBox)
 				return storageBox.StorageBoxType.Name
-			})).
-			AddFieldFn("username", output.FieldFn(func(obj interface{}) string {
+			}).
+			AddFieldFn("username", func(obj any) string {
 				storageBox := obj.(*hcloud.StorageBox)
 				if storageBox.Username == nil {
 					return "-"
 				}
 				return *storageBox.Username
-			})).
-			AddFieldFn("location", output.FieldFn(func(obj interface{}) string {
+			}).
+			AddFieldFn("server", func(obj any) string {
+				storageBox := obj.(*hcloud.StorageBox)
+				if storageBox.Server == nil {
+					return "-"
+				}
+				return *storageBox.Server
+			}).
+			AddFieldFn("system", func(obj any) string {
+				storageBox := obj.(*hcloud.StorageBox)
+				if storageBox.System == nil {
+					return "-"
+				}
+				return *storageBox.System
+			}).
+			AddFieldFn("location", func(obj any) string {
 				storageBox := obj.(*hcloud.StorageBox)
 				return storageBox.Location.Name
-			})).
-			AddFieldFn("size", output.FieldFn(func(obj interface{}) string {
+			}).
+			AddFieldFn("size", func(obj any) string {
 				storageBox := obj.(*hcloud.StorageBox)
-				return strconv.FormatUint(storageBox.Stats.Size, 10)
-			}))
+				if storageBox.Stats == nil {
+					return "-"
+				}
+				return humanize.IBytes(storageBox.Stats.Size)
+			}).
+			AddFieldFn("labels", func(obj any) string {
+				storageBox := obj.(*hcloud.StorageBox)
+				return util.LabelsToString(storageBox.Labels)
+			}).
+			AddFieldFn("created", func(obj any) string {
+				storageBox := obj.(*hcloud.StorageBox)
+				return util.Datetime(storageBox.Created)
+			}).
+			AddFieldFn("age", func(obj any) string {
+				storageBox := obj.(*hcloud.StorageBox)
+				return util.Age(storageBox.Created, time.Now())
+			})
 	},
 }
