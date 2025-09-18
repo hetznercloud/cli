c28e2bf4bbd842b37c1a17202126835ba9097175
phm07 <22707808+phm07@users.noreply.github.com>
2025-09-18 11:06:49 +0200

    feat(storage-box): implement actions (#1145)
    This PR implements actions for Storage Boxes.

--- docs/reference/manual/hcloud_storage-box.md
+++ docs/reference/manual/hcloud_storage-box.md
@@ -26,10 +26,18 @@ Manage Storage Boxes
 
 * [hcloud](hcloud.md)	 - Hetzner Cloud CLI
 * [hcloud storage-box add-label](hcloud_storage-box_add-label.md)	 - Add a label to a Storage Box
+* [hcloud storage-box change-type](hcloud_storage-box_change-type.md)	 - Change type of a Storage Box
 * [hcloud storage-box create](hcloud_storage-box_create.md)	 - Create a new Storage Box
 * [hcloud storage-box delete](hcloud_storage-box_delete.md)	 - Delete a Storage Box
 * [hcloud storage-box describe](hcloud_storage-box_describe.md)	 - Describe a Storage Box
+* [hcloud storage-box disable-protection](hcloud_storage-box_disable-protection.md)	 - Disable resource protection for a Storage Box
+* [hcloud storage-box disable-snapshot-plan](hcloud_storage-box_disable-snapshot-plan.md)	 - Disable automatic snapshots for a Storage Box
+* [hcloud storage-box enable-protection](hcloud_storage-box_enable-protection.md)	 - Enable resource protection for a Storage Box
+* [hcloud storage-box enable-snapshot-plan](hcloud_storage-box_enable-snapshot-plan.md)	 - Enable automatic snapshots for a Storage Box
 * [hcloud storage-box folders](hcloud_storage-box_folders.md)	 - List folders of a Storage Box
 * [hcloud storage-box list](hcloud_storage-box_list.md)	 - List Storage Boxes
 * [hcloud storage-box remove-label](hcloud_storage-box_remove-label.md)	 - Remove a label from a Storage Box
+* [hcloud storage-box reset-password](hcloud_storage-box_reset-password.md)	 - Reset the password of a Storage Box
+* [hcloud storage-box rollback-snapshot](hcloud_storage-box_rollback-snapshot.md)	 - Rolls back the Storage Box to the given Snapshot
 * [hcloud storage-box update](hcloud_storage-box_update.md)	 - Update a Storage Box
+* [hcloud storage-box update-access-settings](hcloud_storage-box_update-access-settings.md)	 - Update access settings of the primary Storage Box account
---
+++ internal/cmd/storagebox/enable_snapshot_plan.go
@@ -0,0 +1,101 @@
+package storagebox
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/hetznercloud/cli/internal/cmd/base"
+	"github.com/hetznercloud/cli/internal/cmd/cmpl"
+	"github.com/hetznercloud/cli/internal/cmd/util"
+	"github.com/hetznercloud/cli/internal/hcapi2"
+	"github.com/hetznercloud/cli/internal/state"
+	"github.com/hetznercloud/hcloud-go/v2/hcloud"
+)
+
+var EnableSnapshotPlanCmd = base.Cmd{
+	BaseCobraCommand: func(client hcapi2.Client) *cobra.Command {
+		cmd := &cobra.Command{
+			Use:   "enable-snapshot-plan [options] <storage-box>",
+			Short: "Enable automatic snapshots for a Storage Box",
+			Long: `Enable automatic snapshots for a Storage Box
+
+Allowed values for --day-of-week are:
+- Sunday, Sun, 0, 7
+- Monday, Mon, 1
+- Tuesday, Tue, 2
+- Wednesday, Wed, 3
+- Thursday, Thu, 4
+- Friday, Fri, 5
+- Saturday, Sat, 6`,
+			ValidArgsFunction: cmpl.SuggestArgs(
+				cmpl.SuggestCandidatesF(client.StorageBox().Names),
+			),
+			TraverseChildren:      true,
+			DisableFlagsInUseLine: true,
+		}
+
+		cmd.Flags().Int("max-snapshots", 0, "Maximum amount of Snapshots that should be created by this Snapshot Plan")
+		_ = cmd.MarkFlagRequired("max-snapshots")
+
+		cmd.Flags().Int("minute", 0, "Minute the Snapshot Plan should be executed on (UTC). Not specified means every minute")
+		cmd.Flags().Int("hour", 0, "Hour the Snapshot Plan should be executed on (UTC). Not specified means every hour")
+		cmd.Flags().String("day-of-week", "", "Day of the week the Snapshot Plan should be executed on. Not specified means every day")
+		cmd.Flags().Int("day-of-month", 0, "Day of the month the Snapshot Plan should be executed on. Not specified means every day")
+
+		_ = cmd.RegisterFlagCompletionFunc("day-of-week", cmpl.SuggestCandidates("monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"))
+
+		// TODO: should we add some validation here to avoid footguns? (e.g. backing up every minute)
+
+		return cmd
+	},
+	Run: func(s state.State, cmd *cobra.Command, args []string) error {
+		idOrName := args[0]
+		maxSnapshots, _ := cmd.Flags().GetInt("max-snapshots")
+		minute, _ := cmd.Flags().GetInt("minute")
+		hour, _ := cmd.Flags().GetInt("hour")
+		dayOfWeek, _ := cmd.Flags().GetString("day-of-week")
+		dayOfMonth, _ := cmd.Flags().GetInt("day-of-month")
+
+		storageBox, _, err := s.Client().StorageBox().Get(s, idOrName)
+		if err != nil {
+			return err
+		}
+		if storageBox == nil {
+			return fmt.Errorf("Storage Box not found: %s", idOrName)
+		}
+
+		opts := hcloud.StorageBoxEnableSnapshotPlanOpts{
+			MaxSnapshots: maxSnapshots,
+		}
+
+		if cmd.Flags().Changed("minute") {
+			opts.Minute = &minute
+		}
+		if cmd.Flags().Changed("hour") {
+			opts.Hour = &hour
+		}
+		if cmd.Flags().Changed("day-of-week") {
+			weekday, err := util.WeekdayFromString(dayOfWeek)
+			if err != nil {
+				return err
+			}
+			opts.DayOfWeek = &weekday
+		}
+		if cmd.Flags().Changed("day-of-month") {
+			opts.DayOfMonth = &dayOfMonth
+		}
+
+		action, _, err := s.Client().StorageBox().EnableSnapshotPlan(s, storageBox, opts)
+		if err != nil {
+			return err
+		}
+
+		if err := s.WaitForActions(s, cmd, action); err != nil {
+			return err
+		}
+
+		cmd.Printf("Snapshot Plan enabled for Storage Box %d\n", storageBox.ID)
+		return nil
+	},
+}
