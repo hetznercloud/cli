331f63c89da326de116ab7b7203da2552adef8c1
phm07 <22707808+phm07@users.noreply.github.com>
2025-09-22 17:02:38 +0200

    feat(storage-box): add subaccount commands (#1160)
    This PR implements the Storage Box Subaccount resource.

---
+++ internal/cmd/storagebox/subaccount/cmpl.go
@@ -0,0 +1,35 @@
+package subaccount
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/spf13/cobra"
+
+	"github.com/hetznercloud/cli/internal/cmd/cmpl"
+	"github.com/hetznercloud/cli/internal/hcapi2"
+)
+
+func SuggestSubaccounts(client hcapi2.Client) cobra.CompletionFunc {
+	return cmpl.SuggestCandidatesCtx(func(cmd *cobra.Command, args []string) []string {
+		if len(args) == 0 {
+			return nil
+		}
+
+		storageBox, _, err := client.StorageBox().Get(cmd.Context(), args[0])
+		if err != nil || storageBox == nil {
+			return nil
+		}
+
+		subaccounts, err := client.StorageBox().AllSubaccounts(context.Background(), storageBox)
+		if err != nil {
+			return nil
+		}
+
+		subaccountIDs := make([]string, 0, len(subaccounts))
+		for _, subaccount := range subaccounts {
+			subaccountIDs = append(subaccountIDs, strconv.FormatInt(subaccount.ID, 10))
+		}
+		return subaccountIDs
+	})
+}
---
+++ internal/cmd/storagebox/subaccount/update_access_settings.go
@@ -0,0 +1,102 @@
+package subaccount
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/spf13/cobra"
+
+	"github.com/hetznercloud/cli/internal/cmd/base"
+	"github.com/hetznercloud/cli/internal/cmd/cmpl"
+	"github.com/hetznercloud/cli/internal/hcapi2"
+	"github.com/hetznercloud/cli/internal/state"
+	"github.com/hetznercloud/hcloud-go/v2/hcloud"
+)
+
+var UpdateAccessSettingsCmd = base.Cmd{
+	BaseCobraCommand: func(client hcapi2.Client) *cobra.Command {
+
+		cmd := &cobra.Command{
+			Use:   "update-access-settings [options] <storage-box> <subaccount>",
+			Short: "Update access settings of the Storage Box Subaccount",
+			ValidArgsFunction: cmpl.SuggestArgs(
+				cmpl.SuggestCandidatesF(client.StorageBox().Names),
+				SuggestSubaccounts(client),
+			),
+			TraverseChildren:      true,
+			DisableFlagsInUseLine: true,
+		}
+
+		cmd.Flags().String("home-directory", "", "Home directory of the Subaccount. Will be created if it doesn't exist yet")
+		cmd.Flags().Bool("enable-samba", false, "Whether the Samba subsystem should be enabled (true, false)")
+		cmd.Flags().Bool("enable-ssh", false, "Whether the SSH subsystem should be enabled (true, false)")
+		cmd.Flags().Bool("enable-webdav", false, "Whether the WebDAV subsystem should be enabled (true, false)")
+		cmd.Flags().Bool("reachable-externally", false, "Whether the Storage Box should be accessible from outside the Hetzner network (true, false)")
+		cmd.Flags().Bool("readonly", false, "Whether the Subaccount should be read-only (true, false)")
+
+		cmd.MarkFlagsOneRequired("enable-samba", "enable-ssh", "enable-webdav", "reachable-externally", "readonly")
+
+		return cmd
+	},
+	Run: func(s state.State, cmd *cobra.Command, args []string) error {
+		idOrName := args[0]
+		homeDirectory, _ := cmd.Flags().GetString("home-directory")
+		enableSamba, _ := cmd.Flags().GetBool("enable-samba")
+		enableSSH, _ := cmd.Flags().GetBool("enable-ssh")
+		enableWebDAV, _ := cmd.Flags().GetBool("enable-webdav")
+		reachableExternally, _ := cmd.Flags().GetBool("reachable-externally")
+		readonly, _ := cmd.Flags().GetBool("readonly")
+
+		storageBox, _, err := s.Client().StorageBox().Get(s, idOrName)
+		if err != nil {
+			return err
+		}
+		if storageBox == nil {
+			return fmt.Errorf("Storage Box not found: %s", idOrName)
+		}
+
+		id, err := strconv.ParseInt(args[1], 10, 64)
+		if err != nil {
+			return fmt.Errorf("invalid Storage Box Subaccount ID: %s", args[1])
+		}
+		subaccount, _, err := s.Client().StorageBox().GetSubaccountByID(s, storageBox, id)
+		if err != nil {
+			return err
+		}
+		if subaccount == nil {
+			return fmt.Errorf("Storage Box Subaccount not found: %d", id)
+		}
+
+		var opts hcloud.StorageBoxSubaccountAccessSettingsUpdateOpts
+		if cmd.Flags().Changed("home-directory") {
+			opts.HomeDirectory = &homeDirectory
+		}
+		if cmd.Flags().Changed("enable-samba") {
+			opts.SambaEnabled = &enableSamba
+		}
+		if cmd.Flags().Changed("enable-ssh") {
+			opts.SSHEnabled = &enableSSH
+		}
+		if cmd.Flags().Changed("enable-webdav") {
+			opts.WebDAVEnabled = &enableWebDAV
+		}
+		if cmd.Flags().Changed("reachable-externally") {
+			opts.ReachableExternally = &reachableExternally
+		}
+		if cmd.Flags().Changed("readonly") {
+			opts.Readonly = &readonly
+		}
+
+		action, _, err := s.Client().StorageBox().UpdateSubaccountAccessSettings(s, subaccount, opts)
+		if err != nil {
+			return err
+		}
+
+		if err := s.WaitForActions(s, cmd, action); err != nil {
+			return err
+		}
+
+		cmd.Printf("Access settings updated for Storage Box Subaccount %d\n", subaccount.ID)
+		return nil
+	},
+}
