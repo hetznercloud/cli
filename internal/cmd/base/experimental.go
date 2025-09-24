0cd8927d82a323e374438724d4bd738868cd4c9e
phm07 <22707808+phm07@users.noreply.github.com>
2025-09-24 12:14:28 +0200

    fix stderr message

--- internal/cmd/base/experimental.go
+++ internal/cmd/base/experimental.go
@@ -11,8 +11,6 @@ import (
 	"github.com/hetznercloud/cli/internal/state/config"
 )
 
-const ExperimentalStderrWarning = "Warning: This command is experimental and may change in the future. Use --no-experimental-warnings to suppress this warning.\n"
-
 // ExperimentalWrapper create a command wrapper that appends a notice to the command
 // descriptions and logs a warning when it is used.
 //
@@ -55,7 +53,7 @@ See %s for more details.
 				return err
 			}
 			if !hideWarning {
-				cmd.PrintErr(ExperimentalStderrWarning)
+				cmd.PrintErrf("Warning: This command is %s and may change in the future. Use --no-experimental-warnings to suppress this warning.\n", maturity)
 			}
 			return nil
 		})
