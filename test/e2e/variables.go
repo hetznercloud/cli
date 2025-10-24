ee289881bde58a03cec771ec398902748a0c208a
phm07 <22707808+phm07@users.noreply.github.com>
2025-10-24 17:03:51 +0200

    docs: replace deprecated Server Type (#1211)
    The cpx_1 and cx_2 Server Types have been deprecated and are soon no
    longer available. This PR updates all references to these Server Types
    to only include supported Server Types.

--- test/e2e/variables.go
+++ test/e2e/variables.go
@@ -16,10 +16,10 @@ var (
 	TestImageID = getEnv("TEST_IMAGE_ID", "161547269")
 
 	// TestServerType is the default server type used in end-to-end tests.
-	TestServerType = getEnv("TEST_SERVER_TYPE", "cpx11")
+	TestServerType = getEnv("TEST_SERVER_TYPE", "cpx22")
 
 	// TestServerTypeUpgrade is the upgrade server type used in end-to-end tests.
-	TestServerTypeUpgrade = getEnv("TEST_SERVER_TYPE_UPGRADE", "cpx21")
+	TestServerTypeUpgrade = getEnv("TEST_SERVER_TYPE_UPGRADE", "cpx32")
 
 	// TestArchitecture is the default architecture used in end-to-end tests, should match the architecture of the TestServerType.
 	TestArchitecture = getEnv("TEST_ARCHITECTURE", string(hcloud.ArchitectureX86))
@@ -28,13 +28,13 @@ var (
 	TestLoadBalancerTypeName = getEnv("TEST_LOAD_BALANCER_TYPE_NAME", "lb11")
 
 	// TestDatacenterName is the default datacenter name where we execute our end-to-end tests.
-	TestDatacenterName = getEnv("TEST_DATACENTER_NAME", "nbg1-dc3")
+	TestDatacenterName = getEnv("TEST_DATACENTER_NAME", "hel1-dc2")
 
 	// TestDatacenterID is the default datacenter ID where we execute our end-to-end tests (Must be the ID of TestDatacenterName)
-	TestDatacenterID = getEnv("TEST_DATACENTER_ID", "2")
+	TestDatacenterID = getEnv("TEST_DATACENTER_ID", "3")
 
 	// TestLocationName is the default location where we execute our end-to-end tests.
-	TestLocationName = getEnv("TEST_LOCATION", "nbg1")
+	TestLocationName = getEnv("TEST_LOCATION", "hel1")
 
 	// TestISOName is the default ISO used for testing
 	TestISOName = getEnv("TEST_ISO_NAME", "SW_DVD9_Win_Server_STD_CORE_2022__64Bit_German_DC_STD_MLF_X22-74292.iso")
