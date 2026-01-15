9916cf43daa85792f9d31e1aee38167d947937fe
phm07 <22707808+phm07@users.noreply.github.com>
2026-01-15 17:05:02 +0100

    feat: build .deb, .rpm and .apk release files

---
+++ scripts/manpages.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra/doc"
+
+	"github.com/hetznercloud/cli/internal/cli"
+	"github.com/hetznercloud/cli/internal/state"
+	"github.com/hetznercloud/cli/internal/state/config"
+)
+
+func main() {
+	if err := os.MkdirAll("./manpages", 0755); err != nil {
+		log.Fatal(err)
+	}
+
+	s, _ := state.New(config.New())
+	rootCommand := cli.NewRootCommand(s)
+
+	err := doc.GenManTree(rootCommand, nil, "./manpages")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Man pages generated successfully")
+}
